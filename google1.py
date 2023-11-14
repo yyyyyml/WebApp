75206b93db02ff5b05af0c8f04fef419cf529a3b
孟林 杨 <1762849856@qq.com>
2023-11-14 14:39:12 +0800

    refactor

--- google1.py
+++ google1.py
@@ -67,3 +67,18 @@ def google_scholar_search(query, pn=1):
     else:
         print("Error fetching search results.")
         return []
+    
+if __name__ == "__main__":
+    query = "Deep Learning"
+    page = 1
+    results = google_scholar_search(query, page)
+
+    for i, result in enumerate(results):
+        print(f"Result {i+1}:")
+        print(f"Title: {result['title']}")
+        print(f"Link: {result['link']}")
+        print(f"Snippet: {result['snippet']}")
+        print(f"Authors: {result['authors']}")
+        print(f"Citations: {result['citations']}")
+        print(f"PDF Link: {result['pdf_link']}")
+        print("\n")
